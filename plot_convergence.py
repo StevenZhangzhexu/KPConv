9dec30b22aba98a038cb0cefa7be27f2693718cf
StevenZhangzhexu <stevenzhang@alteredverse.net>
2023-10-23 17:49:38 +0800

    update

--- plot_convergence.py
+++ plot_convergence.py
@@ -715,7 +715,7 @@ def experiment_name_1():
 
     # Using the dates of the logs, you can easily gather consecutive ones. All logs should be of the same dataset.
     start = 'Log_2023-10-18_17-09-56'
-    end = 'Log_2023-10-19_17-17-26'
+    end = 'Log_2023-10-20_17-07-16'
 
     # Name of the result path
     res_path = 'results'
